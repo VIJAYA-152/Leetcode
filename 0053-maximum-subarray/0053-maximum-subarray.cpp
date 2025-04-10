0c3bb6730b733b9f8b28e40cea6e1de8897f28b3
VIJAYA-152 <vijayalaxmi1528@gmail.com>
2025-04-10 21:54:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxi=nums[0];
+    int maxProduct(vector<int>& nums) {
+        int maxi=INT_MIN;
         int n=nums.size();
-         
-        int current=nums[0];
-        for(int i=0;i<n;i++){
-            current=max(nums[i],nums[i]+current);
-            maxi=max(current,maxi);
-        }
-        return maxi;
+         int pre=1,suff=1;
+         for(int i=0;i<n;i++){
+            if(pre==0)  pre=1;
+            if(suff==0)  suff=1;
+            pre=pre*nums[i];
+            suff=suff*nums[n-i-1];
+            maxi=max(maxi,max(pre,suff));
+         }
+         return maxi;
     }
 };
